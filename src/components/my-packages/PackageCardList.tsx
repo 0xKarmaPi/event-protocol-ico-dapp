558892576f908c53912442f8537eeb1de07494c4
TonyVu <hocvanvu1999@gmail.com>
2024-08-09 14:34:41 +0700

    fix: add key props when render list placeholder

--- src/components/my-packages/PackageCardList.tsx
+++ src/components/my-packages/PackageCardList.tsx
@@ -62,7 +62,7 @@ export default function PackageCardList() {
 		return (
 			<div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
 				{[...Array(6)].map((_, index) => (
-					<Card className="space-y-5 p-4" radius="lg">
+					<Card key={index} className="space-y-5 p-4" radius="lg">
 						<Skeleton className="rounded-lg">
 							<div className="bg-default-300 h-[200px] rounded-lg"></div>
 						</Skeleton>
